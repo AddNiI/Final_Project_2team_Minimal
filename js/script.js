e69a0e77991a8e386dd65233763bead655b989e6
Your name <kola.kolya876@gmail.com>
2024-01-04 18:37:21 +0200

    04.01.24

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".clients-cards-block").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      }
+    ]
+  });
+});
